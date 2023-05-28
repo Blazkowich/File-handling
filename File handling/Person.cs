903f2cbe862c1efeb0e4657e0ecd4cf0a0021209
Oto <Oto@OTAR>
2023-05-29 00:04:54 +0400

    Add project files.

---
+++ File handling/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+namespace PersonInfo
+{
+    #region Person Structure
+    class Person
+    {
+        StreamWriter sw;
+        StreamReader sr;
+
+        public string nameg;
+        public string surname;
+        public string sex;
+        public int age;
+        public string fileDir;
+
+    }
+    #endregion
+    class Information
+    {
+
+        #region Write File
+        static void WriteFile()
+        {
+            StreamWriter sw = new StreamWriter(fileDir);
+            sw.WriteLine("Name: " + name);
+            sw.WriteLine("Surname: " + surname);
+            sw.WriteLine("Sex: " + sex);
+            sw.WriteLine("Age: " + age.ToString());
+            sw.Close();
+        }
+        #endregion
+
+        #region Read File
+        static void ReadFile()
+        {
+            StreamReader sr = new StreamReader(fileDir, true);
+
+            List<string> lines = new List<string>();
+
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            foreach (string textLine in lines)
+            {
+                Console.WriteLine(textLine);
+            }
+
+        }
+        #endregion
+
+        public Information()
+        {
+            nam
+        }
+
+    }
+    
+}
---
+++ File handling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using PersonInfo;
+namespace File_handling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        PersonInfo.Person PersonInformation = new PersonInfo.Person();
+
+        PersonInformation.fileDir = @"C:\Users\LEGION Y520\Desktop\Text\Info.txt";
+        PersonInformation.age = 15;
+        PersonInformation.sex = "Male";
+        PersonInformation.name = "Vajiko";
+        PersonInformation.surname = "Kokoradze";
+
+        
+
+
+        PersonInformation.WriteFile();
+        PersonInformation.ReadFile();
+
+    }
+}
