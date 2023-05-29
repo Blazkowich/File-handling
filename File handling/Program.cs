95108a9e0e222c68d73a42b78a2106776783cb6a
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-29 20:52:44 +0400

    Update Program.cs

--- File handling/Program.cs
+++ File handling/Program.cs
@@ -1,21 +1,26 @@
 using WriteOrRead;
 using PersonInfo;
+using System.Xml.Linq;
+using System;
+
 namespace FileHandling;
 class Program
 {
     static void Main(string[] args)
     {
 
-
         PersonInformation personInformation = new PersonInformation("Vajiko","Kokoradze","Mamakacuri",15);
+      
+
+        InformationChange informationChange = new InformationChange(@"/Users/otar/Desktop/Person Information/log.txt");
 
 
 
-        InformationChange.Write(personInformation) ;
+        informationChange.Write(personInformation);
 
-
-
-        PersonInformation.DisplayInfo();
+        informationChange.Read();
+        
+        Console.WriteLine($"Name: {personInformation.name}\nSurname: {personInformation.surname}\nAge: {personInformation.age}\nSex: {personInformation.sex}");
         
     }
 }
