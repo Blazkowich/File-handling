3b4145c99c2152bd2bf1b0efbba4865ccb57ee79
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-29 14:29:07 +0400

    Update Program.cs

--- File handling/Program.cs
+++ File handling/Program.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.IO;
+using WriteOrRead;
 using PersonInfo;
-namespace File_handling;
-
+namespace FileHandling;
 class Program
 {
     static void Main(string[] args)
     {
+        int age = 15;
+        string sex = "Male";
+        string name = "Vajiko";
+        string surname = "Kokoradze";
 
-        PersonInfo.Person PersonInformation = new PersonInfo.Person();
+        PersonInformation personInformation = new PersonInformation();
+        personInformation.age = age;
+        personInformation.name = name;
+        personInformation.surname = surname;
+        personInformation.sex = sex;
+        InformationChange.Write(personInformation) ;
 
-        PersonInformation.fileDir = @"C:\Users\LEGION Y520\Desktop\Text\Info.txt";
-        PersonInformation.age = 15;
-        PersonInformation.sex = "Male";
-        PersonInformation.name = "Vajiko";
-        PersonInformation.surname = "Kokoradze";
 
+
+        PersonInformation recoverInformation = new PersonInformation();
+        string fullInfo = InformationChange.Read();
+        string[] array = fullInfo.Split(",");
+        recoverInformation.name = array[0];
+        recoverInformation.surname = array[1];
+        recoverInformation.age = int.Parse(array[2]);
+        recoverInformation.sex = array[3];
         
 
-
-        PersonInformation.WriteFile();
-        PersonInformation.ReadFile();
-
+        Console.WriteLine($"Name: {array[0]}\nSurname: {array[1]}\nAge: {array[2]}\nSex: {array[3]}");
     }
 }
+
