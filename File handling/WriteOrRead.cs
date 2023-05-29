87dc74d729d7790e23de140b506d2711226607ac
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-29 17:16:20 +0400

    Update WriteOrRead.cs

--- File handling/WriteOrRead.cs
+++ File handling/WriteOrRead.cs
@@ -6,7 +6,7 @@ namespace WriteOrRead
     public static class InformationChange
     {
         public static string FilePath;
-
+        #region Write in Document
         public static void Write(PersonInformation person)
         {
             StreamWriter sw = new StreamWriter(FilePath);
@@ -14,22 +14,24 @@ namespace WriteOrRead
             sw.Close();
 
         }
+        #endregion
 
+        #region Read from File
         public static string Read()
         { 
-
             StreamReader sr = new StreamReader(FilePath);
 
             string readInformation = sr.ReadLine();
             sr.Close();
             return readInformation;
-
         }
+        #endregion
 
+        #region Direction Constructor
         static InformationChange()
         {
             FilePath = @"/Users/otar/Desktop/Person Information/log.txt";
         }
+        #endregion
     }
 }
-
