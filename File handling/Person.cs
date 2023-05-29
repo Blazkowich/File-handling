2f15334cca4b7d7999d5c7b3f6d4869ffa4d8d33
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-29 14:28:44 +0400

    Update Person.cs

--- File handling/Person.cs
+++ File handling/Person.cs
@@ -1,69 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.IO;
-using System.Xml.Linq;
-
+using WriteOrRead;
 namespace PersonInfo
 {
-    #region Person Structure
-    class Person
-    {
-        StreamWriter sw;
-        StreamReader sr;
+	public class PersonInformation
+	{ 
+		public string name { get; set; }
+        public string surname { get; set; }
+        public string sex { get; set; }
+        public int age { get; set; }
 
-        public string nameg;
-        public string surname;
-        public string sex;
-        public int age;
-        public string fileDir;
-
-    }
-    #endregion
-    class Information
-    {
-
-        #region Write File
-        static void WriteFile()
-        {
-            StreamWriter sw = new StreamWriter(fileDir);
-            sw.WriteLine("Name: " + name);
-            sw.WriteLine("Surname: " + surname);
-            sw.WriteLine("Sex: " + sex);
-            sw.WriteLine("Age: " + age.ToString());
-            sw.Close();
-        }
-        #endregion
-
-        #region Read File
-        static void ReadFile()
-        {
-            StreamReader sr = new StreamReader(fileDir, true);
-
-            List<string> lines = new List<string>();
-
-            string line;
-
-            while ((line = sr.ReadLine()) != null)
-            {
-                lines.Add(line);
-            }
-
-            foreach (string textLine in lines)
-            {
-                Console.WriteLine(textLine);
-            }
-
-        }
-        #endregion
-
-        public Information()
-        {
-            nam
-        }
-
-    }
-    
+	}
 }
+
