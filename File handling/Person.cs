0554dd91f81ad60c35b0d0f02ad9381e4f7aab4d
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-30 13:49:36 +0400

    Update Person.cs

--- File handling/Person.cs
+++ File handling/Person.cs
@@ -3,23 +3,23 @@ using System.IO;
 using WriteOrRead;
 namespace PersonInfo
 {
-    #region Basic Information get / set
     public class PersonInformation
-	{ 
+    {  
 		public string name { get; set; }
         	public string surname { get; set; }
         	public string sex { get; set; }
         	public int age { get; set; }
-        #endregion
+        	public List<PersonInformation> Child { get; set; }
 
-        #region Constructor for information merging
+
         public PersonInformation(string name, string surname, string sex, int age)
         {
             this.name = name;
             this.surname = surname;
             this.sex = sex;
             this.age = age;
+            Child = new List<PersonInformation>();
+
         }
-        #endregion
     }
 }
