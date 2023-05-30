a5131d0424bec188f8c04d5f651465706178dd00
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-30 13:48:55 +0400

    Update Program.cs

--- File handling/Program.cs
+++ File handling/Program.cs
@@ -8,20 +8,24 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        //Enter information here ... 
         PersonInformation personInformation = new PersonInformation("Vajiko","Kokoradze","Mamakacuri",15);
       
-
+        //Code Line for File Direction
         InformationChange informationChange = new InformationChange(@"/Users/otar/Desktop/Person Information/log.txt");
 
 
-
+        //Write function call
         informationChange.Write(personInformation);
 
+
+        //Read function call
         informationChange.Read();
-        
+
+
+        //Read from .txt file
         Console.WriteLine($"Name: {personInformation.name}\nSurname: {personInformation.surname}\nAge: {personInformation.age}\nSex: {personInformation.sex}");
-        
+
     }
 }
 
