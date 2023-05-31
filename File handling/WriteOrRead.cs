cb9413281ffcd23f9f49d2ec6e0be6d759c3d6d1
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-05-31 22:36:28 +0400

    Update WriteOrRead.cs

--- File handling/WriteOrRead.cs
+++ File handling/WriteOrRead.cs
@@ -1,39 +1,39 @@
 using System;
 using System.IO;
 using PersonInfo;
+
 namespace WriteOrRead
 {
     public class InformationChange
     {
         public string FilePath { get; set; }
 
+
+        public void Write(PersonInformation[] persons)
+        {
+            StreamWriter sw = new StreamWriter(FilePath);
+            foreach (var person in persons)
+            {
+                string personData = $"{person.name}, {person.surname}, {person.sex}, {person.age}";
+                sw.WriteLine(personData);
+            }
+            sw.Close();
+        }
+
+
+        public string[] Read()
+        {
+            StreamReader sr = new StreamReader(FilePath);
+            string readInformation = sr.ReadToEnd();
+            string[] lines = readInformation.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            sr.Close();
+            return lines;
+        }
+
+
         public InformationChange(string path)
         {
             FilePath = path;
-        }
-
-        public void Write(PersonInformation person)
-        {
-            StreamWriter sw = new StreamWriter(FilePath);
-            sw.WriteLine($"{person.name},{person.surname},{person.age},{person.sex}");
-            sw.Close();
-
-        }
-
-        public string Read()
-        { 
-            StreamReader sr = new StreamReader(FilePath);
-
-            string readInformation = sr.ReadLine();
-            string[] array = readInformation.Split(",");
-
-            string name = array[0];
-            string surname = array[1];
-            int age = int.Parse(array[2]);
-            string sex = array[3];
-
-            sr.Close();
-            return readInformation;
         }
     }
 }
